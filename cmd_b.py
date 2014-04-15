fd546836d9ec8c877c0db52e5abfa64d0dceec61
Simon <simonroy@thoughtworks.com>
2014-04-15 22:57:26 +0530

    Simon - Complete bindings.

--- cmd_b.py
+++ cmd_b.py
@@ -5,16 +5,48 @@ import os.path
 from .common import *
 
 index={}
+matches_from_index = []
+selected_word = ""
 
 class TheNavigatorCommand(sublime_plugin.WindowCommand):
     def run(self):
-        fs=getallfiles(cwd_for_window(self.window))
-        for f in fs:
-            build_index(f)
-        print(index)
-        
+        global matches_from_index
+        global selected_word
+        view = self.window.active_view()
+        selected_word=view.substr(view.word(view.sel()[0]))
+        if(len(index) == 0):
+            fs=getallfiles(cwd_for_window(self.window))
+            for f in fs:
+                build_index(f)    
+        matches_from_index = index.get(selected_word)
+        if(matches_from_index == None):
+            return
+        if(len(matches_from_index) == 1):
+            model = matches_from_index[0]
+            self.window.open_file(model[2])
+            navigate(self.window.active_view(), model)
+        elif(len(matches_from_index) > 1):
+            self.show_matches(selected_word)
 
-# def build_index():
+    def show_matches(self, function_name):
+        names = []
+        for match in matches_from_index:
+            item = []
+            item.append(function_name + ":" + match[1])
+            item.append(match[2])
+            names.append(item)
+        self.window.show_quick_panel(names, self.match_selected)
+
+    def match_selected(self, match):
+        global matches_from_index
+        model = matches_from_index[match]
+        self.window.open_file(model[2])
+        navigate(self.window.active_view(), model)
+
+def navigate(view, model):
+    view.sel().clear()
+    view.sel().add(sublime.Region(model[0], model[0]))
+    view.show_at_center(model[0]) 
 
 def getallfiles(dir, extn = ".r"):
     fs=[]
@@ -25,10 +57,10 @@ def getallfiles(dir, extn = ".r"):
     return fs
 
 def build_index(file):
+    global index
     f=open(file,"r")
     text = f.read()
     search_pattern = r"([a-zA-Z0-9\_]+) *(=|<-|<<-) *(function\(.*?\))"
-    print(file)
     for match in re.finditer(search_pattern, text):
         index[match.group(1)] = index.get(match.group(1)) or []
         index[match.group(1)].append([match.start(),match.group(3),file])
